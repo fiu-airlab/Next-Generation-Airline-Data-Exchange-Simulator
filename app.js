e53e8ae6e1a2e3bcb691da0fa2d9c39b4b9551b6
Isaac Reynaldo <ireynaldo@farelogix.com>
2019-02-13 16:30:38 -0500

    User authentication

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.set('view engine', 'ejs');
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Header',
-    'Origin, X-Requested-With, Content-Type, Accept');
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
 });
